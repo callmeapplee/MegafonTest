3e8def616a1e9ba0b0fd3fa8fa1a37ba622e0336
Boturbek <boturgithub@gmail.com>
2025-01-25 20:08:22 +0500

    Initial Commit

---
+++ MegafonTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MegafonTest
+//
+//  Created by Ботурбек Имомдодов on 25/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
